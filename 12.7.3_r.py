3d4125558e704569e3a349c5f593752774e6ec62
AnaOla <olaresko.a@gmail.com>
2022-07-21 22:20:14 +0300

    убран лишний пробелв последнем print

--- 12.7.3_r.py
+++ 12.7.3_r.py
@@ -3,4 +3,4 @@ per_cent_list = list(map(float, per_cent.values()))
 money = int(input('Введите сумму:'))
 deposite = [round(i/100*money) for i in per_cent_list]
 print(deposite)
-print('Максимальная сумма, которую вы можете заработать —','',max(deposite))
+print('Максимальная сумма, которую вы можете заработать —',max(deposite))
